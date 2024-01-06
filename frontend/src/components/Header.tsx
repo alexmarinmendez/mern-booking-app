3abf177a2f10564183bb4541a8893ff8c24f577b
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-01-06 08:30:57 -0500

    fix(FRONT): change Link -> span in order to debug Link bug

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,16 @@
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
         <div className="bg-blue-800 py-6">
             <div className="container mx-auto flex justify-between">
                 <span className="text-3xl text-white font-bold tracking-tight">
-                    <Link to="/">MernHolidays.com</Link>
+                    {/* <Link to="/">MernHolidays.com</Link> */}
+                    MernHolidays.com
                 </span>
                 <span className="flex space-x-2">
-                    <Link to="/sign-in" className="flex items-center text-blue-600 px-3 font-bold hover:bg-gray-100 hover:text-green-500">Sign In</Link>
+                    {/* <Link to="/sign-in" className="flex items-center text-blue-600 px-3 font-bold hover:bg-gray-100 hover:text-green-500">Sign In</Link> */}
+                    <span className="flex items-center text-blue-600 px-3 font-bold hover:bg-gray-100 hover:text-green-500">Sign In</span>
                 </span>
             </div>
         </div>
